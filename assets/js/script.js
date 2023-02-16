50a27fb356488d90a1a827ecb2eb98ec4c2707c8
SevenRooT <seven.root.stevens@gmail.com>
2023-02-15 21:33:50 -0800

    building out funcitions for collectScores, creating question and answer list objects, coding dynamically created page items

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,23 @@ let testQuestions = document.getElementById("questions");
 // these variable will hold the test answers
 let testAnswers = document.getElementById("answers");
 
-// these variables create elements to be displayed after clock runs out
-let finalTitle = document.createElement("h2");
+// these variables create elements on the Final Score page
+let scoreTitle = document.createElement("h2");
 let finalScore = document.createElement("p");
 let initialsField = document.createElement("input");
 let scoreSubmit = document.createElement("button");
+
+// these variables create elements on the High Scores page
+let highScores = document.createElement("h2");
+let scoresList = document.createElement("ul");
+let startAgain = document.createElement("button");
+let clearScores = document.createElement("button");
+// variables set to empty, 0, blank, or false
+let scoreCount = 0;
+let latestScore = 0;
+
+let timer;
+let timerCount;
 
 // the test question object
 
@@ -31,25 +43,68 @@ const answerList = {
  answers4: ["somewhat equal", "strictly equal", "equal", "arrow function"]
 }
 
-// After: "All done!", "Your Final Score", initials text field, submit button
-// hiding this screen's elements until timer runs out
-if (gameTimer === 0) {
+// The init function is called when the page loads 
+function init() { }
+
+// startTest function is called when the "start" button is clicked
+
+// startTimer function is called in startTest
+function startTimer() {
+ timer = setInterval(function () {
+  timerCount--;
+ })
+}
+
+/* answerCheck function is connected to the #checkAnswer in html, and is called after one of the answers is clicked and checked*/
+
+/* function to set scores in DOM, including a counting method for continual tallying*/
+// create an array or retrieve the existing scores from localStorage
+function collectScores() {
+ let scores = JSON.parse(localStorage.getItem("gameScores")) || [];
+
+ // an object to hold the key.values pairs of scoreCount and score
+ let newScore = {};
+
+ // populating the gameScores array
+ scores.scoreCount = scoreCount;
+ scores.latestScore = latestScore;
+ scores.push(newScore);
+
+ // Store the updated array back into localStorage
+ localStorage.setItem("gameScores", JSON.stringify(scores));
+
+ // looping through "gameScores" to identify highest score
+ let highScore = 0;
+ for (let key in scores) {
+  let val = scores[key];
+  if (val > highScore) {
+   highScore = val;
+  }
+ }
+}
+
+
+
+/* scripts relating to when timer runs out -- screen changes to "All done!", "Your Final Score", initials text field, submit button */
+
+//hiding this screen's elements until timer runs out
+if (timerCount === 0) {
 
 }
 // this needs to be inside of a function
 //function must clear screen first
-const finalScreen = () => {
+const scoreScreen = () => {
 
 
  // code to generate the new title "All Done!"
 
- finalTitle.setAttribute("id", "finalTitle");
- finalTitle.textContent = "All Done!";
+ scoreTitle.setAttribute("id", "scoreTitle");
+ scoreTitle.textContent = "All Done!";
 
  // code to generate final score <p>
 
  finalScore.setAttribute("id", "finalScore");
- finalScore.textContent = "Your Final Score " + "";
+ finalScore.textContent = "Your Final Score " + highScore;
 
  // code to generate the initials field 
 
@@ -63,7 +118,18 @@ const finalScreen = () => {
 
  // appends all final information to the #quizContainer from html
 
- quizContainer.appendChild(finalTitle);
+ quizContainer.appendChild(scoreTitle);
  quizContainer.appendChild(finalScore); //needs a function 
  quizContainer.appendChild(initialsField); //needs a function
 }
+
+/* scripts associate with the High Scores page, including the "Start Again" and "Clear High Scores" buttons */
+highScores.setAttribute("id", "highScores");
+
+scoresList.setAttribute("id", "scoresList");
+
+startAgain.setAttribute("type", "button");
+startAgain.setAttribute("id", "startAgain");
+
+clearScores.setAttribute("type", "button");
+clearScores.setAttribute("id", "clearScores");
